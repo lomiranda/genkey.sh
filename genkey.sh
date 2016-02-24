cea48645b31e3e9acbecd231840cf9637951c57c
Lucas Miranda <lomiranda92@gmail.com>
2016-02-24 11:41:15 -0300

    Indenting code

--- genkey.sh
+++ genkey.sh
@@ -6,7 +6,7 @@
 
 #If the arguments are null, show help
 if [ -z "$1" ] || [ -z "$2" ]; then
-        echo ""
+	echo ""
         echo "USAGE: genkey.sh [arguments] [username]"
         echo ""
         echo "Arguments:"
@@ -39,54 +39,54 @@ while test $# -gt 0; do
                                 echo ""
                                 exit 0
                                 ;;
-
+                                
                         --generate)
                                 shift
-
+                                
                                 #Creates the user and the key paths if they do not exist
                                 id -u $USERNAME &>/dev/null || useradd $USERNAME
                                 mkdir -p $KEYPATH
                                 mkdir -p $AUTHPATH
 								
-								#Sets the proper permission to the user keys' folder
+				#Sets the proper permission to the user keys' folder
                                 chown -R $USERNAME $KEYPATH
-
+                                
                                 #Creates the private and public keys with the username in comment
                                 su $USERNAME -c "ssh-keygen -t rsa -N '' -f $KEY -n $USERNAME -C $KEYCOMMENT"
-
+                                
                                 #Inserts the new public key in the authorized_keys
                                 cat $PUBKEY >> $AUTHFILE
 								
-								#Sets the proper permission to the user keys' folder again
+				#Sets the proper permission to the user keys' folder again
                                 chown -R $USERNAME:$USERNAME $AUTHPATH
-
+                                
                                 #Sync the authorized_keys with the others server instances. It also creates the user and his .ssh directory
                                 for IPADD in `cat $SERVERS | egrep -v '^(#|$)'`; do
                                         rsync -ravzp --rsync-path="id -u $USERNAME &>/dev/null || useradd $USERNAME && mkdir -p $AUTHPATH && rsync" $AUTHFILE root@$IPADD:$AUTHFILE
                                 done
-
+                                
                                 shift
                                 ;;
-
+                                
                         --delete)
                                 shift
-								
-								#Remove the line the has the username from the authorized_keys
+                                
+				#Remove the line the has the username from the authorized_keys
                                 sed --i "/$KEYCOMMENT/d" $AUTHFILE
-
+                                
                                 #Sync the authorized_keys with the others server instances.
                                 for IPADD in `cat $SERVERS | egrep -v '^(#|$)'`; do
                                         rsync -ravzp $AUTHFILE root@$IPADD:$AUTHFILE
                                 done
-								
-								#Deletes the keys' directory of the user
+                                
+				#Deletes the keys' directory of the user
                                 rm -rf $KEYPATH
-
+                                
                                 shift
                                 ;;
 
                         *)
-								echo ""
+				echo ""
                                 echo "Invalid Argument!"
                                 echo "Run \"bash genkey.sh -h\" to show all arguments"
                                 echo ""
